f4948c8946cb2fdd01beb045c4c740e2d9b701de
miniopxd <minipro787@gmail.com>
2023-09-04 23:24:38 +0700

    uwu

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-const Emoji = ["😎", "🤓", "🤩", "😏", "😼"][R(5)]
+const Emoji = ["😎", "🤓", "🤩", "😏", "😼"][R(5)];
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.to('progress', {
+gsap.to("progress", {
   value: 100,
-  ease: 'none',
-  scrollTrigger: { scrub: 0.3 }
+  ease: "none",
+  scrollTrigger: { scrub: 0.3 },
 });
 
 const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion)");
@@ -17,17 +16,19 @@ const updateMarqueeText = () => {
   [...marquees].forEach((marquee) => {
     marquee.classList.add("active");
 
-    marquee.addEventListener("webkitTransitionEnd", () => FText(marquee, marqueeText));
-    marquee.addEventListener("transitionend", () => FText(marquee, marqueeText));
+    marquee.addEventListener("webkitTransitionEnd", () =>
+      FText(marquee, marqueeText)
+    );
+    marquee.addEventListener("transitionend", () =>
+      FText(marquee, marqueeText)
+    );
   });
 };
 
 const FText = (marquee, marqueeText) => {
-  console.log("FText");
   marquee.classList.remove("active");
   marquee.innerText = `${marqueeText} ${Emoji} `.repeat(30);
-}
-
+};
 
 const updateBgColor = (color) =>
   document.documentElement.style.setProperty("--color-background", color);
@@ -47,9 +48,9 @@ if (!prefersReducedMotion.matches) {
       scrub: 0.25,
       start: "top bottom",
       end: "bottom top",
-      ease: "power2"
+      ease: "power2",
     },
-    xPercent: -50
+    xPercent: -50,
   });
 }
 
@@ -65,7 +66,7 @@ gsap.utils.toArray(".page-content__section h2").forEach((heading) => {
       setActiveSection(heading.parentElement),
     onEnterBack: () =>
       marqueeText !== heading.textContent &&
-      setActiveSection(heading.parentElement)
+      setActiveSection(heading.parentElement),
   });
 });
 
@@ -73,5 +74,21 @@ function R(Max = 1) {
   return Math.floor(Math.random() * Max);
 }
 
-console.log("GSAP ScrollTrigger Cr. https://codepen.io/hexagoncircle/pen/xxwBLMy\nScroll progress Cr.https://codepen.io/Mamboleoo/pen/abdwYaJ");
+console.log(
+  "GSAP ScrollTrigger Cr. https://codepen.io/hexagoncircle/pen/xxwBLMy\nScroll progress Cr.https://codepen.io/Mamboleoo/pen/abdwYaJ"
+);
 console.log("ขอบคุณที่เข้ามาเยี่ยมชม \nEmail: contact@ikrt.dev");
+
+// var x = 0;
+
+// setInterval(() => {
+//   const policyCardTitles = document.getElementsByClassName("policy-card-title");
+//   for (let index = 0; index < policyCardTitles.length; index++) {
+//     const policyCard = policyCardTitles[index];
+//     policyCard.style.backgroundImage = `linear-gradient(${x}deg, #f60, rgb(123, 3, 197))`;
+//     x += 1;
+//     if (x > 720) {
+//       x = 0;
+//     }
+//   }
+// }, 1000);
